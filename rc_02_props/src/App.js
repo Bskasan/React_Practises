09c514eb3dac330ef6aebb7fe16137b614d8a8ac
Bskasan <b.kasan@yandex.com>
2023-02-18 16:24:40 +0200

    getting data by map method from data.js

--- rc_02_props/src/App.js
+++ rc_02_props/src/App.js
@@ -2,29 +2,33 @@
 //import './App.css';
 
 import Person from "./components/Person";
+import { data } from "./data";
 
 //? Props is the parameters in React and is object in RCT.js
 function App() {
   return (
     <div>
-      <h1> React js </h1>
-      <Person
-        name="Jonathan Klaus"
-        img="https://media.istockphoto.com/id/1433419992/fi/valokuva/nuori-mies-rennossa-sinisess%C3%A4-paidassa-ja-lasissa-pitelem%C3%A4ss%C3%A4-puhelinta-katsomassa-kameraa.jpg?s=612x612&w=0&k=20&c=Y8JQUnqTEMG1Yn83crSfFO7rzI40MGb69dDJhb-WjFU="
-        tel="+49 233 44 55"
-      />
-      <Person
-        name="Josh Steven"
-        img="https://cdn.pixabay.com/photo/2016/11/29/01/34/man-1866572_960_720.jpg"
-        tel="+49 233 44 55"
-      />
-      <Person
-        name="Emily Lerk"
-        img="https://media.istockphoto.com/id/1389348844/photo/studio-shot-of-a-beautiful-young-woman-smiling-while-standing-against-a-grey-background.jpg?b=1&s=170667a&w=0&k=20&c=bHXqxhYseYfmPLqYPOAbfGlv1Ye8d9KeUraRzad5XCg="
-        tel="+49 233 44 55"
-      />
+      {data.map((item) => (
+        <Person name={item.name} img={item.img} tel={item.tel}/>
+      ))}
     </div>
   );
 }
 
 export default App;
+
+// <Person
+//     name="Emily Kurnikov"
+//     img="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__480.jpg"
+//     tel="+49 233 44 55"
+//   />
+//   <Person
+//     name="Josh Steven"
+//     img="https://cdn.pixabay.com/photo/2016/11/18/19/07/happy-1836445__340.jpg"
+//     tel="+49 233 44 55"
+//   />
+//   <Person
+//     name="Sumany Kirinihni"
+//     img="https://cdn.pixabay.com/photo/2015/01/27/09/58/man-613601__340.jpg"
+//     tel="+49 233 44 55"
+//   />
---
+++ rc_02_props/src/data.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    name: "Jonathan Klaus",
+    img: "https://media.istockphoto.com/id/1433419992/fi/valokuva/nuori-mies-rennossa-sinisess%C3%A4-paidassa-ja-lasissa-pitelem%C3%A4ss%C3%A4-puhelinta-katsomassa-kameraa.jpg?s=612x612&w=0&k=20&c=Y8JQUnqTEMG1Yn83crSfFO7rzI40MGb69dDJhb-WjFU=",
+    tel: "+49 233 44 55",
+  },
+  {
+    name: "Josh Steven",
+    img: "https://cdn.pixabay.com/photo/2016/11/29/01/34/man-1866572_960_720.jpg",
+    tel: "+49 233 44 55",
+  },
+  {
+    name: "Emily Lerk",
+    img: "https://media.istockphoto.com/id/1389348844/photo/studio-shot-of-a-beautiful-young-woman-smiling-while-standing-against-a-grey-background.jpg?b=1&s=170667a&w=0&k=20&c=bHXqxhYseYfmPLqYPOAbfGlv1Ye8d9KeUraRzad5XCg=",
+    tel: "+49 233 44 55",
+  },
+];
