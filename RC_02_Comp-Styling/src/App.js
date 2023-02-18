551b38e80c0981b315bf734c43b0d5fbcbfce64c
Bskasan <b.kasan@yandex.com>
2023-02-18 11:07:06 +0200

    header component exported and imported

--- RC_02_Comp-Styling/src/App.js
+++ RC_02_Comp-Styling/src/App.js
@@ -1,13 +1,14 @@
 //import logo from './logo.svg';
 //import './App.css';
 
+import Header from "./components/Header";
+
 //* This is App Component, as we know react is component-based.
 function App() {
   return (
     //* JSX ( JavaScript and XML )
     <div>
-      <h1> Hi React !</h1>
-      <h2> This is JSX </h2>
+      <Header />
     </div>
   );
 }
