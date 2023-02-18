ec07d2db2751bf9622d7260e96a51d2f0c186716
Bskasan <b.kasan@yandex.com>
2023-02-18 15:47:08 +0200

    props is object in React

--- rc_02_props/src/App.js
+++ rc_02_props/src/App.js
@@ -1,10 +1,15 @@
 //import logo from './logo.svg';
 //import './App.css';
+import Msg from "./components/Msg";
 
+//? Props is the parameters in React and is object in RCT.js
 function App() {
   return (
     <div>
       <h1> React js </h1>
+      <Msg name="Canon" />
+      <Msg name="John" />
+      <Msg name="Micheal" />
     </div>
   );
 }
---
+++ rc_02_props/src/components/Msg.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Msg = (props) => {
+  return <div>Hi {props.name}</div>;
+};
+
+export default Msg;
