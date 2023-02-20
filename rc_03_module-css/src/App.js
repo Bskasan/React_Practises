5ff3b9dcdfa97b34253d498795ea295372619208
Bskasan <b.kasan@yandex.com>
2023-02-20 19:27:46 +0200

    documents for rc_03 created 1

---
+++ rc_03_module-css/src/App.js
@@ -0,0 +1,23 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+
+//! 3rd Party Libraries
+//! - Bootstrap, React-Bootstrap
+//! - Tailwindd
+//! - SCSS / SASS
+//! - Styled Components / Emotion / Compiled Styled
+//! - AntDesign
+//! - Material UI ( Google )
+//! - Semantic UI
+
+
+function App() {
+  return (
+    <div>
+     <h1> Module CSS </h1>
+    </div>
+  );
+}
+
+export default App;
