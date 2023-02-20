c8bba3124efa92c052cd615b4870535fc14d5803
Bskasan <b.kasan@yandex.com>
2023-02-20 21:17:01 +0200

    comments added

--- rc_03_module-css/src/components/button/Button.jsx
+++ rc_03_module-css/src/components/button/Button.jsx
@@ -1,5 +1,12 @@
 import ButtonStyle from "./Button.module.css";
 
+//* Eğer CSS'deki class adi kebap-case seklinde yazilmis ise
+//* module ile bu class'a erismek icin ornekte goruldugu gibi
+//* koseli parentez ile erisim yapilir.Koseli parentez erisimi
+//* en garanti yontemdir.
+
+//* ORN:moduleName["class-adi"]
+
 const Button = ({ btn }) => {
   return (
     <div className={ButtonStyle["title"]}>
--- rc_03_module-css/src/components/card/Card.jsx
+++ rc_03_module-css/src/components/card/Card.jsx
@@ -1,6 +1,12 @@
 import Button from "../button/Button";
 import CardStyle from "./Card.module.css";
 
+//* module.css'de derleyici CSS class'larini alarak bunlari unique olacak
+//* sekilde yeniden adlandirir. (Ornek: card_title__XaSde)
+//* Bu durumda, global scope problemi ortadan kalktigi icin stillerin
+//* baska class tarafindan ezilmesi (overriding) engellenir.
+//* CCS module Webpack, Browsify gibi tool'lar ile kullanilabilir.
+
 const Card = ({ lang, img, btn }) => {
   return (
     <div>
