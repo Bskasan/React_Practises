8285187277e6377c7fc88d9670742fdda44d204b
Bskasan <b.kasan@yandex.com>
2023-02-20 22:17:22 +0200

    card jsx done

--- rc_03_sass/src/components/Card.jsx
+++ rc_03_sass/src/components/Card.jsx
@@ -2,10 +2,27 @@ import React from "react";
 
 import CardStyle from "../scss/card.module.scss";
 
-const Card = () => {
+//! Data is js document --> Curly Braces.
+const Card = ({ data }) => {
+  console.log(data);
   return (
-    <div>
-      <h1>Card</h1>
+    <div className={CardStyle.container}>
+      {data.map((item) => {
+        //! Destructuring...
+        const { id, img, comment, job, name } = item;
+        return (
+          <div key={id} className={CardStyle.card}>
+            <h1>{name}</h1>
+            <h3>{job}</h3>
+            <p>{comment}</p>
+            <img src={img} alt={name} />
+            <div>
+              <button className={CardStyle["btn-small"]}>Small</button>
+              <button className={CardStyle["btn-large"]}>Large</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
