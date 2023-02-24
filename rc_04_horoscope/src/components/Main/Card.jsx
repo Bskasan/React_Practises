6c74db721d9626036751bc5b3276adef10995a5e
Bskasan <b.kasan@yandex.com>
2023-02-24 21:01:38 +0200

    created card component

---
+++ rc_04_horoscope/src/components/Main/Card.jsx
@@ -0,0 +1,9 @@
+const Card = () => {
+  return (
+    <div className="cards">
+      <h1>Card</h1>
+    </div>
+  );
+};
+
+export default Card;
--- rc_04_horoscope/src/components/Main/Main.jsx
+++ rc_04_horoscope/src/components/Main/Main.jsx
@@ -1,19 +1,14 @@
 import "./Main.scss";
 import { data } from "../../helpers/data";
+import Card from "./Card";
+
 
 const Main = () => {
   console.log(data);
   return (
     <div className="card-container">
       <h1>Card</h1>
-      {data.map((item) => {
-        return (
-          <div className="card">
-            {item.title}
-            <img src={item.image} alt="" />
-          </div>
-        );
-      })}
+      {data.map((item) => <Card />)}
     </div>
   );
 };
