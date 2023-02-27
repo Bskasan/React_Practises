ab0ec7a14b010698b64fa6a02289418cdf3a047f
Bskasan <b.kasan@yandex.com>
2023-02-27 21:45:33 +0200

    classComponent 2

--- rc_05_events-usestatehook/src/components/2-classComponent/Counter.jsx
+++ rc_05_events-usestatehook/src/components/2-classComponent/Counter.jsx
@@ -12,12 +12,33 @@
 import React from "react";
 
 export class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+
+    // this.increment = function () {}
+
+    //? First Method - Binding (For Increment Method)
+    this.increment = this.increment.bind(this);
+  }
+
+  increment() {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+
   render() {
     return (
       <div className="container text-center mt-4">
         <h2 className="text-danger">Class Components</h2>
-        <h1 className="display-4">Count: 0</h1>
-        <button className="btn btn-success">INC</button>
+        <h1 className="display-4">Count: {this.state.count}</h1>
+        <button onClick={this.increment} className="btn btn-success">
+          INC
+        </button>
         <button className="btn btn-danger">CLEAR</button>
         <button className="btn btn-warning">DEC</button>
       </div>
