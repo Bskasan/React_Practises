518aa4a981744dfc04731a35c6ca0f65c6b51e1c
Bskasan <b.kasan@yandex.com>
2023-02-27 22:24:55 +0200

    use state counter 2

--- rc_05_events-usestatehook/src/App.js
+++ rc_05_events-usestatehook/src/App.js
@@ -1,11 +1,13 @@
 import Event from "./components/1-events/Event";
 import Counter from "./components/2-classComponent/Counter";
+import UseStateCounter from "./components/3-useStateHook/UseStateCounter";
 
 function App() {
   return (
     <div>
       <Event />
       <Counter />
+      <UseStateCounter />
     </div>
   );
 }
--- rc_05_events-usestatehook/src/components/3-useStateHook/UseStateCounter.jsx
+++ rc_05_events-usestatehook/src/components/3-useStateHook/UseStateCounter.jsx
@@ -17,3 +17,30 @@
 //*    (Custom hook'lar icerisinde bir hook cagrilabilir)
 //?    https://reactjs.org/docs/hooks-rules.html
 //* =============================================================
+
+import { useState } from "react";
+
+const UseStateCounter = () => {
+  //? You can think of like this is a global variable.
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="container text-center mt-4">
+      <h2 className="text-danger">Use State Counter</h2>
+      <h1 className="display-4">Count: {count}</h1>
+      <button onClick={increment} className="btn btn-success">
+        INC
+      </button>
+      <button className="btn btn-danger">CLR</button>
+      <button onClick={() => setCount(count - 1)} className="btn btn-warning">
+        DEC
+      </button>
+    </div>
+  );
+};
+
+export default UseStateCounter;
