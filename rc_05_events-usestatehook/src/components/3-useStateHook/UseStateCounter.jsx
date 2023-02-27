b9fbbaea5249d1010569107ab74b31f8039a3b5a
Bskasan <b.kasan@yandex.com>
2023-02-27 21:57:59 +0200

    classComponent done

---
+++ rc_05_events-usestatehook/src/components/3-useStateHook/UseStateCounter.jsx
@@ -0,0 +1,19 @@
+//* ==================== HOOKS ===============================
+//! Hook'lar fonksiyonel component'ler icerisinde state'leri kullanmamiza
+//! olanak saglayan ozel fonksiyonlardir.
+//! React 16.8 versiyonu ile gelmistir ve geldikten sonra Class-componentler'in
+//! kullanimi cok azaltmistir.
+
+//? React'ta useState(), useEffect(), useContext() gibi bir cok built-in
+//? Hook bulunmaktadir. Ayrica custom hook tanimlamak da mumkundur.
+
+//* Hook Kullanim Kurallari:
+//* 1. İlk olarak import edilmeliler. import { useState } from "react";
+//* 2. Hook'lar ust seviyede kullanilmalidir. Yani Hook'lar bir
+//*    dongunun, kosul cumleciginin ve icice fonksiyonlarin icerisinde
+//*    kullanilmamalidir.
+//* 3. Hook'lar sadece React Fonksiyonel componentleri icerisinde cagrilmalidir.
+//*    Normal Javascript fonksiyonlari icerisinde cagrilmamalidir
+//*    (Custom hook'lar icerisinde bir hook cagrilabilir)
+//?    https://reactjs.org/docs/hooks-rules.html
+//* =============================================================
