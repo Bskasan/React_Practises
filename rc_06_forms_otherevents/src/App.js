ef36cee43f332e70674ee19709631fe5f731cbd9
Bskasan <b.kasan@yandex.com>
2023-02-28 17:49:33 +0200

    mouse event 1

--- rc_06_forms_otherevents/src/App.js
+++ rc_06_forms_otherevents/src/App.js
@@ -1,6 +1,6 @@
 //import Form from "./components/1-forms/Form";
-import KeyboardEvents from "./components/2-keyboardClipboardEvents/KeyboardClipbord";
-//import MouseEvent from "./components/3-mouseEvents/MouseEvent";
+//import KeyboardEvents from "./components/2-keyboardClipboardEvents/KeyboardClipbord";
+import MouseEvent from "./components/3-mouseEvents/MouseEvent";
 //import FormWithObject from "./components/1-forms/FormWithObject";
 
 function App() {
@@ -8,8 +8,8 @@ function App() {
     <div className="container mt-4 text-center">
       {/* <Form /> */}
       {/* <FormWithObject /> */}
-      <KeyboardEvents />
-      {/* <MouseEvent /> */}
+      {/* <KeyboardEvents /> */}
+      <MouseEvent />
     </div>
   );
 }
--- rc_06_forms_otherevents/src/components/3-mouseEvents/MouseEvent.jsx
+++ rc_06_forms_otherevents/src/components/3-mouseEvents/MouseEvent.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MouseEvent = () => {
-  return <div>MouseEvent</div>;
+  //? let visible = false;
+
+  const [visible, setVisible] = useState(false);
+  const [toggle, setToggle] = useState(false);
+
+  console.log(toggle);
+
+  const handleDoubleClick = (e) => {
+    console.log(e);
+    setToggle(!toggle);
+  };
+
+  return (
+    <div className="container text-center d-flex flex-column align-items-center mt-4">
+      <h2 className="text-danger">MOUSE EVENTS</h2>
+
+      <div
+        id="todo-1"
+        className="bg-success text-light w-50 p-4"
+        onMouseOver={() => setVisible(true)}
+        onMouseOut={() => setVisible(false)}
+      >
+        todo item 1
+      </div>
+
+      {/* Conditional Rendering */}
+      {visible && <div>Mouse Events</div>}
+
+      <div
+        id="todo-2"
+        className="bg-success text-light w-50 p-4 mt-4"
+        onDoubleClick={handleDoubleClick}
+      >
+        todo item 2
+      </div>
+
+      <div id="todo-3" className="bg-success text-light w-50 p-4 my-4">
+        todo item 3
+      </div>
+
+      <p>X and Y</p>
+
+      <p className="text-danger fw-bold">
+        {"X"}
+        {"Y"}
+      </p>
+    </div>
+  );
 };
 
 export default MouseEvent;
