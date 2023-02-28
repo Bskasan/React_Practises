afc95e72c25e2126076fd05f63bc87d681ad84c3
Bskasan <b.kasan@yandex.com>
2023-02-28 14:35:32 +0200

    setInputData

--- rc_06_forms_otherevents/src/components/2-keyboardClipboardEvents/KeyboardClipbord.jsx
+++ rc_06_forms_otherevents/src/components/2-keyboardClipboardEvents/KeyboardClipbord.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const KeyboardClipbord = () => {
-  return <div>KeyboardClipbord</div>;
+  const [inputData, setInputData] = useState("");
+
+  return (
+    <div>
+      <h2 className="display-5 text-danger">Keyboard-Clipboard Event</h2>
+
+      <input
+        type="text"
+        className="form-control p-3 fs-3"
+        onChange={(e) => {
+          //! Whatever you write, it will be uppercase.
+          e.target.value = e.target.value.toLocaleUpperCase();
+          setInputData(e.target.value);
+        }}
+      />
+
+      <p className="text-start mt-4 fs-3">{inputData.toUpperCase()}</p>
+
+      <textarea
+        className="form-control"
+        name="textarea"
+        id="textarea"
+        cols="30"
+        rows="10"
+      ></textarea>
+    </div>
+  );
 };
 
 export default KeyboardClipbord;
