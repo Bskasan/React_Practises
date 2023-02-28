78791c2c11e68e476d6a3a975bc6035c72f4dec2
Bskasan <b.kasan@yandex.com>
2023-02-28 13:11:32 +0200

    formwithobject started

--- rc_06_forms_otherevents/src/App.js
+++ rc_06_forms_otherevents/src/App.js
@@ -1,11 +1,13 @@
 import Form from "./components/1-forms/Form";
 import KeyboardEvents from "./components/2-keyboardClipboardEvents/KeyboardClipbord";
 import MouseEvent from "./components/3-mouseEvents/MouseEvent";
+import FormWithObject from "./components/1-forms/FormWithObject";
 
 function App() {
   return (
     <div className="container mt-4 text-center">
-      <Form />
+      {/* <Form /> */}
+      <FormWithObject />
       {/* <KeyboardEvents />
       <MouseEvent /> */}
     </div>
---
+++ rc_06_forms_otherevents/src/components/1-forms/FormWithObject.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+
+const FormWithObject = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert(`
+    username: ${username}
+    email: ${email}
+    password: ${password}
+    `);
+
+    //* Debug on Console
+    console.log(`
+    username: ${username}
+    email: ${email}
+    password: ${password}
+    `);
+
+    setEmail("");
+    setUsername("");
+    setPassword("");
+  }
+
+  return (
+    <div>
+      <h2 className="display-6 text-danger">FORMS</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">
+            Username : <span>{username}</span>
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            onChange={handleUsername}
+            value={username}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email : <span>{email}</span>
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            required
+          />
+        </div>
+
+        <button type="submit" className="btn btn-danger p-3">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default FormWithObject;
