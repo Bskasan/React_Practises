8113c1d5e0f70d7782d3307ab0091b64f35a184c
Bskasan <b.kasan@yandex.com>
2023-02-28 13:04:44 +0200

    setPassword and setEmail added

--- rc_06_forms_otherevents/src/components/1-forms/Form.jsx
+++ rc_06_forms_otherevents/src/components/1-forms/Form.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 
 const Form = () => {
   const [username, setUsername] = useState("-------");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("name.surname@bskdev.com");
+  const [password, setPassword] = useState("****************");
 
   const handleUsername = (e) => {
     setUsername(e.target.value);
@@ -27,20 +27,27 @@ const Form = () => {
         </div>
         <div className="mb-3">
           <label htmlFor="email" className="form-label">
-            Email
+            Email : <span>{email}</span>
           </label>
           <input
             type="email"
             className="form-control"
             id="email"
-            aria-describedby="emailHelp"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">
             Password
           </label>
-          <input type="password" className="form-control" id="password" />
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
         </div>
 
         <button type="submit" className="btn btn-danger p-3">
