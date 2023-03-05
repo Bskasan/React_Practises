9aaae2348eadf00a7e5ac98ad3c648741b02bfeb
Bskasan <b.kasan@yandex.com>
2023-03-05 20:57:42 +0200

    search feature added and project done

--- rc_07_nba-legends/src/components/CardContainer.jsx
+++ rc_07_nba-legends/src/components/CardContainer.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import { Container, Col, Row, Form } from "react-bootstrap";
 import { data } from "../helpers/data";
 import PlayerCard from "./PlayerCard";
 
 const CardContainer = () => {
+  const [search, setSearch] = useState("");
+  console.log(search);
   return (
     <>
-      <Form.Control placeholder="Search Player..." className="w-50 m-auto"/>
+      <Form.Control
+        placeholder="Search Player..."
+        type="search"
+        className="w-50 m-auto"
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <Container className="card-container rounded-4 my-4 p-3">
         <Row className="g-3 justify-content-center">
-          {data.map((player, i) => (
-            <Col md={6} lg={4} xl={3} key={i}>
-              <PlayerCard {...player} />
-            </Col>
-          ))}
+          {data
+            .filter((player) =>
+              player.name.toLowerCase().includes(search.trim().toLowerCase())
+            )
+            .map((player, i) => (
+              <Col md={6} lg={4} xl={3} key={i}>
+                <PlayerCard {...player} />
+              </Col>
+            ))}
         </Row>
       </Container>
     </>
