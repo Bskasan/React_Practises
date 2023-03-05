3fb6e93a77a9eb4dd29b6b59930b81b64020aca7
Bskasan <b.kasan@yandex.com>
2023-03-05 20:13:37 +0200

    player card hover structure added

--- rc_07_nba-legends/src/components/CardContainer.jsx
+++ rc_07_nba-legends/src/components/CardContainer.jsx
@@ -5,9 +5,9 @@ import PlayerCard from "./PlayerCard";
 const CardContainer = () => {
   return (
     <Container className="card-container rounded-4 my-4 p-3">
-      <Row>
+      <Row className="g-3 justify-content-center">
         {data.map((player, i) => (
-          <Col key={i}>
+          <Col md={6} lg={4} xl={3} key={i}>
             <PlayerCard {...player} />
           </Col>
         ))}
--- rc_07_nba-legends/src/components/PlayerCard.jsx
+++ rc_07_nba-legends/src/components/PlayerCard.jsx
@@ -4,8 +4,8 @@ import { Card } from "react-bootstrap";
 const PlayerCard = ({ name, img, statistics }) => {
   //const { name, img, statistics } = props;
   return (
-    <Card>
-      <Card.Img variant="top" src={img} />
+    <Card className="rounded-2 m-auto player-card" role="button">
+      <Card.Img className="player-img" variant="top" src={img} />
       <Card.Footer>
         <Card.Title>{name}</Card.Title>
       </Card.Footer>
