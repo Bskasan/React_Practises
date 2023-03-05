06587532e104a4b4ce4593dfa3292c3d58bb0c2c
Bskasan <b.kasan@yandex.com>
2023-03-05 19:02:44 +0200

    Module not found problem solved and Header component done

--- rc_07_nba-legends/src/App.js
+++ rc_07_nba-legends/src/App.js
@@ -1,11 +1,10 @@
-import Container from "react-bootstrap/Container";
+import { Container } from "react-bootstrap";
 //import CardContainer from "./components/CardContainer";
 import Header from "./components/Header";
 
-
 function App() {
   return (
-    <Container>
+    <Container className="text-center mt-4">
       <Header />
     </Container>
   );
--- rc_07_nba-legends/src/components/Header.jsx
+++ rc_07_nba-legends/src/components/Header.jsx
@@ -1,12 +1,11 @@
-import Container from 'react-bootstrap/Container';
-import Image from "react-bootstrap/Image";
+import { Container, Image } from "react-bootstrap";
 import nbaLogo from "../assets/nba-logo.png";
 
 const Header = () => {
   return (
     <Container>
       <Image src={nbaLogo} width="200px"></Image>
-      <h1>NBA Legends</h1>
+      <h1 className="my-2 font-monospace display-4 fw-bold">NBA LEGENDS</h1>
     </Container>
   );
 };
