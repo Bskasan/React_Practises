c086ab64aa1723f293e4ed8ef76fbba4b649d4f9
Bskasan <b.kasan@yandex.com>
2023-03-05 20:32:34 +0200

    onClick method added

--- rc_07_nba-legends/src/components/PlayerCard.jsx
+++ rc_07_nba-legends/src/components/PlayerCard.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 
 const PlayerCard = ({ name, img, statistics }) => {
+  const [showImg, setShowImg] = useState(true);
+
   //const { name, img, statistics } = props;
+
   return (
-    <Card className="rounded-2 m-auto player-card" role="button">
-      <Card.Img className="player-img" variant="top" src={img} />
+    <Card
+      onClick={() => setShowImg(!showImg)}
+      className="rounded-2 m-auto player-card"
+      role="button"
+    >
+      {showImg ? (
+        <Card.Img className="player-img" variant="top" src={img} />
+      ) : (
+        <ul className="m-auto">
+          {statistics.map((item, i) => {
+            return (
+              <li key={i} className="h5 text-start list-unstyled">
+                🏀 {item}
+              </li>
+            );
+          })}
+        </ul>
+      )}
       <Card.Footer>
         <Card.Title>{name}</Card.Title>
       </Card.Footer>
