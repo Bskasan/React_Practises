88433dac0eae3103a24b60f664423a57bd2782be
Bskasan <b.kasan@yandex.com>
2023-03-08 21:33:46 +0200

    getUser added

--- rc_08_lifecycle-useeffect/src/components/User.jsx
+++ rc_08_lifecycle-useeffect/src/components/User.jsx
@@ -1,13 +1,35 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 const User = () => {
+  //let user = "Sample";
+
+  const [user, setUser] = useState("");
+
+  const getUser = () => {
+    fetch("https://randomuser.me/api/")
+      .then((res) => res.json())
+      .then((data) => setUser(data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  console.log(user);
+
   return (
     <div>
-      <h1>NAME</h1>
+      {/* <h1>{user.name.first}</h1> */}
       <img src="" alt="" />
       <h4>email</h4>
       <h5>phone</h5>
       <p>dateOfBirth</p>
+
+      <button onClick={getUser} className="btn btn-warning">
+        Get User
+      </button>
     </div>
   );
 };
