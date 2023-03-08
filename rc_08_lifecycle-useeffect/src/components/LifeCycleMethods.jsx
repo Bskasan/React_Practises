5038ee58aa4c9012f7d6c589d901eb965fb58772
Bskasan <b.kasan@yandex.com>
2023-03-08 18:49:53 +0200

    documents created

---
+++ rc_08_lifecycle-useeffect/src/components/LifeCycleMethods.jsx
@@ -0,0 +1,17 @@
+//?=============================================================
+//?                 LIFECYCLE METOTLARI
+//?      https://reactjs.org/docs/react-component.html
+//?=============================================================
+
+//* Lifecycle metotlari componetnlerin DOM'da varoldugu sure boyunca
+//* uzerinde islem yapmamizi imkan saglayan ozel React mototlaridir.
+//* Ornegin bir component olsuturuldugunda, DOM'a basilsiginda,
+//* guncellendiginde veya DOM'dan kaldirildiginda bir seyler yapmak icin
+//* lifecycle metotlari kullanilabilir.
+//* En bilindik lifecycle metodu render() metodudur
+
+//* Bir component'in olsuturulmasi (constructor,
+//* Bir componentin DOM agacina eklenmesinin sonrasi(componentDidMount)
+//* Bir component'in DOM'a basilmasi (render)
+//* (Optional)Bir componentin guncellenmesinin sonrasi (componentDidUpdate)
+//* Bir component'in DOM agacindan kaldirilmasi sonrasi(componentWillUnmount)
