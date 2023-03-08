2d5bf14f3de9a6b8ba8032fdb0e87c3413f88d7b
Bskasan <b.kasan@yandex.com>
2023-03-08 21:47:33 +0200

    optional chaining

--- rc_08_lifecycle-useeffect/src/components/User.jsx
+++ rc_08_lifecycle-useeffect/src/components/User.jsx
@@ -4,28 +4,33 @@ import { useState, useEffect } from "react";
 const User = () => {
   //let user = "Sample";
 
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState();
 
   const getUser = () => {
     fetch("https://randomuser.me/api/")
       .then((res) => res.json())
-      .then((data) => setUser(data))
+      .then((data) => setUser(data.results[0]))
       .catch((error) => console.log(error));
   };
 
   useEffect(() => {
+    console.log("MOUNTING");
     getUser();
   }, []);
 
+  console.log("RENDER");
   console.log(user);
 
   return (
     <div>
-      {/* <h1>{user.name.first}</h1> */}
-      <img src="" alt="" />
-      <h4>email</h4>
-      <h5>phone</h5>
-      <p>dateOfBirth</p>
+      <h1>
+        {/*! Optional Chaining */}
+        {user?.name?.first} {user?.name?.last}
+      </h1>
+      <img src={user?.picture?.large} alt="profile-picture" />
+      <h4>{user?.email}</h4>
+      <h5>{user?.phone}</h5>
+      <p>{user?.dob?.date}</p>
 
       <button onClick={getUser} className="btn btn-warning">
         Get User
