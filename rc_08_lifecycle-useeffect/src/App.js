d76b71dcec4633e4dc1a446d0a0b2192b8a4f587
Bskasan <b.kasan@yandex.com>
2023-03-08 21:21:18 +0200

    user jsx started

--- rc_08_lifecycle-useeffect/src/App.js
+++ rc_08_lifecycle-useeffect/src/App.js
@@ -1,17 +1,20 @@
 //import LifeCycleMethods from "./components/LifeCycleMethods";
 import { useState } from "react";
-import UseEffectHook from "./components/UseEffectHook";
+//import UseEffectHook from "./components/UseEffectHook";
+import User from "./components/User";
 
 function App() {
-  const [toggle, setToggle] = useState(false);
+  // const [toggle, setToggle] = useState(false);
 
   return (
     <div className="container text-center mt-4">
-      <button className="btn btn-success" onClick={() => setToggle(!toggle)}>
+      {/* <button className="btn btn-success" onClick={() => setToggle(!toggle)}>
         {toggle ? "Hide" : "Show"}
-      </button>
+      </button> */}
       {/* {toggle ? <LifeCycleMethods /> : null} */}
-      {toggle ? <UseEffectHook /> : null}
+      {/* {toggle ? <UseEffectHook /> : null} */}
+
+      <User />
     </div>
   );
 }
---
+++ rc_08_lifecycle-useeffect/src/components/User.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const User = () => {
+  return (
+    <div>
+      <h1>NAME</h1>
+      <img src="" alt="" />
+      <h4>email</h4>
+      <h5>phone</h5>
+      <p>dateOfBirth</p>
+    </div>
+  );
+};
+
+export default User;
