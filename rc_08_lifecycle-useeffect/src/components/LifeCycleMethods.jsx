7c3b8d16e2afc35a4f8bc06438116fb2a3034e70
Bskasan <b.kasan@yandex.com>
2023-03-08 20:10:34 +0200

    component will unmount and did update

--- rc_08_lifecycle-useeffect/src/components/LifeCycleMethods.jsx
+++ rc_08_lifecycle-useeffect/src/components/LifeCycleMethods.jsx
@@ -19,7 +19,7 @@
 import React from "react";
 
 class LifeCycleMethods extends React.Component {
-     //! 1-) Bir componentin olsuturulmasinda cagrilir
+  //! 1-) Bir componentin olsuturulmasinda cagrilir
   constructor(props) {
     console.log("1 - Constructor running");
     super(props);
@@ -39,6 +39,20 @@ class LifeCycleMethods extends React.Component {
   //! Her yasam dongusu icin bir kere calisir.
   componentDidMount() {
     console.log("3 - Component Did Mount Method!!!");
+  }
+
+  //! 4-) Bu metot ilk render haric diger tüm render'lardan sonra cagrilir.
+  //!  prevState ve prevProps degerlerini parametre olarak alabilir.
+  componentDidUpdate(prevState) {
+    console.log("4-componentDidUpdate");
+    // if(prevState.dolarParite - this.dolarParite > 3){
+    //     SendEmail()
+    // }
+  }
+
+  //! 5-) Bir component DOM agacindan kaldiriltiktan hemen sonra cagirlir.
+  componentWillUnmount() {
+    console.log("5-componentWillUnmount");
   }
 
   render() {
