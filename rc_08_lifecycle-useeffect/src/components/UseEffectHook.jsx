ea08528c721fd553cb836fe319ea3244985274ab
Bskasan <b.kasan@yandex.com>
2023-03-08 20:32:43 +0200

    useEffectHook notes added

---
+++ rc_08_lifecycle-useeffect/src/components/UseEffectHook.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+
+//?===============================================
+//?                USEEFFECT HOOK
+//?===============================================
+//! UseEffect Hook'u fonksiyonel componenler'te yan etkileri
+//! (side effect) gerceklestirmek icin kullanilir.
+//! componentDidMount,componentDidUpdate,ve componentWillUnmount
+//! metotlarinin bir birlesimi gibi dusunulebilir.
+
+//! useEffect(() => {
+//*   /* ComponentDidMount code */
+//*      Kodlar
+//! }, []);
+
+//! useEffect(() => {
+//*   */ ComponentDidMount + componentDidUpdate code */
+//! }, [state1, state2]);
+
+//! useEffect(() => {
+//?   /* ComponentDidMount code */
+//!   return () => {
+//*     //* componentWillUnmount code */
+//!   };
+//! }, []);
+
+//! useEffect(() => {
+//*   //* componentDidMount code + componentDidUpdate code */
+
+//!   return () => {
+//*     //* componentWillUnmount code */
+//!   };
+//! }, [state1, state2]); //? Dependency Array
+
+const UseEffectHook = () => {
+  return <div></div>;
+};
+
+export default UseEffectHook;
