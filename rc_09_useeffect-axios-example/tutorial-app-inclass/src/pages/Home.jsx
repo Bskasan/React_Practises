303ff6fa021dbc6d847bb6cdddaa22a3e583d40a
Bskasan <b.kasan@yandex.com>
2023-03-10 18:17:51 +0200

    getTutorials from API method added

--- rc_09_useeffect-axios-example/tutorial-app-inclass/src/pages/Home.jsx
+++ rc_09_useeffect-axios-example/tutorial-app-inclass/src/pages/Home.jsx
@@ -1,12 +1,23 @@
+import axios from "axios";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
 
-const Home = () => {
+const Home = async () => {
+  const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+
+  const getTutorials = async () => {
+    //! Destructuring
+    const { data } = await axios(BASE_URL);
+    console.log(data);
+  };
+
+  getTutorials();
+
   return (
-    <div>
+    <>
       <AddTutorial />
       <TutorialList />
-    </div>
+    </>
   );
 };
 
