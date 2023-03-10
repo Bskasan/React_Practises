9d2215bd6bcea5b9ae3107066586b1f3b439b8ee
Bskasan <b.kasan@yandex.com>
2023-03-10 18:30:52 +0200

    useEffect and tutorials added

--- rc_09_useeffect-axios-example/tutorial-app-inclass/src/components/TutorialList.jsx
+++ rc_09_useeffect-axios-example/tutorial-app-inclass/src/components/TutorialList.jsx
@@ -4,20 +4,20 @@ import { FaEdit } from "react-icons/fa";
 import { AiFillDelete } from "react-icons/ai";
 import axios from "axios";
 
-const TutorialList = () => {
-  const tutorials = [
-    {
-      id: 1,
-      title: "JS",
-      description: "JS is a programming language.",
-    },
-    {
-      id: 2,
-      title: "React",
-      description: "JS library for UI design",
-    },
-  ];
+// const tutorials = [
+//     {
+//       id: 1,
+//       title: "JS",
+//       description: "JS is a programming language.",
+//     },
+//     {
+//       id: 2,
+//       title: "React",
+//       description: "JS library for UI design",
+//     },
+//   ];
 
+const TutorialList = ({ tutorials }) => {
   return (
     <div className="container mt-4">
       <table className="table table-striped">
--- rc_09_useeffect-axios-example/tutorial-app-inclass/src/pages/Home.jsx
+++ rc_09_useeffect-axios-example/tutorial-app-inclass/src/pages/Home.jsx
@@ -1,23 +1,33 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
 
-const Home = async () => {
+const Home = () => {
+  const [tutorials, setTutorials] = useState([]);
+
   const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
 
   const getTutorials = async () => {
     //! Destructuring
     const { data } = await axios(BASE_URL);
-    console.log(data);
+    setTutorials(data);
   };
 
-  getTutorials();
+  console.log(tutorials);
+
+  //? componentDidMount
+  useEffect(() => {
+    getTutorials();
+  }, []);
+
+  //getTutorials();
 
   return (
-    <>
+    <div>
       <AddTutorial />
-      <TutorialList />
-    </>
+      <TutorialList tutorials={tutorials} />
+    </div>
   );
 };
 
