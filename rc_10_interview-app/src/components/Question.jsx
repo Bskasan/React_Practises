b183f42928b7445110ec0d31da43e6595d67e662
Bskasan <b.kasan@yandex.com>
2023-03-14 21:14:06 +0200

    show if else statement added

--- rc_10_interview-app/src/components/Question.jsx
+++ rc_10_interview-app/src/components/Question.jsx
@@ -1,13 +1,35 @@
 import { useState } from "react";
+import { arrowdown, arrowup } from "../helper/icons";
 
 const Question = ({ id, question, answer }) => {
   //   console.log("item", props);
 
   const [show, setShow] = useState(false);
 
-  return <div className="card-group">
-    
-  </div>;
+  return (
+    <div className="card-group">
+      {show ? (
+        <div className="card">
+          <div className="ques-answer">
+            <h5>
+              {id}.{question}
+            </h5>
+            <button>{arrowup}</button>
+          </div>
+          <p>{answer}</p>
+        </div>
+      ) : (
+        <div className="card">
+          <div className="ques-answer">
+            <h5>
+              {id}.{question}
+            </h5>
+            <button>{arrowdown}</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Question;
