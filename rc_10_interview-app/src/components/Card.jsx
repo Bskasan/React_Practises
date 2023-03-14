14d97887212096c03659b70911d8ff9315b99a68
Bskasan <b.kasan@yandex.com>
2023-03-14 21:34:30 +0200

    interview q app done

---
+++ rc_10_interview-app/src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Card = ({ id, question, answer, icon, handleToggle }) => {
+  console.log("answer", answer);
+
+  return (
+    <div className="card">
+      <div className="ques-answer">
+        <h5>
+          {id}.{question}
+        </h5>
+        <button onClick={handleToggle}>{icon}</button>
+      </div>
+      {answer && <p>{answer}</p>}
+    </div>
+  );
+};
+
+export default Card;
--- rc_10_interview-app/src/components/Question.jsx
+++ rc_10_interview-app/src/components/Question.jsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
 import { arrowdown, arrowup } from "../helper/icons";
+import Card from "./Card";
 
 const Question = ({ id, question, answer }) => {
   //   console.log("item", props);
 
   const [show, setShow] = useState(false);
 
+  const handleToggle = () => setShow(!show);
+
   return (
     <div className="card-group">
-      {show ? (
+      {/* {show ? (
         <div className="card">
           <div className="ques-answer">
             <h5>
               {id}.{question}
             </h5>
-            <button>{arrowup}</button>
+            <button onClick={handleToggle}>{arrowup}</button>
           </div>
           <p>{answer}</p>
         </div>
@@ -24,9 +27,37 @@ const Question = ({ id, question, answer }) => {
             <h5>
               {id}.{question}
             </h5>
-            <button>{arrowdown}</button>
+            <button onClick={handleToggle}>{arrowdown} </button>
           </div>
         </div>
+      )} */}
+      {/*2.yöntem*/}
+      {/* <div className="card">
+        <div className="ques-answer">
+          <h5>
+            {id}.{question}
+          </h5>
+          <button onClick={handleToggle}>{show ? arrowdown : arrowup}</button>
+        </div>
+        {show && <p>{answer}</p>}
+      </div>
+    </div> */}
+      {/*3.yöntem*/}
+      {show ? (
+        <Card
+          id={id}
+          question={question}
+          answer={answer}
+          icon={arrowup}
+          handleToggle={handleToggle}
+        />
+      ) : (
+        <Card
+          id={id}
+          question={question}
+          icon={arrowdown}
+          handleToggle={handleToggle}
+        />
       )}
     </div>
   );
