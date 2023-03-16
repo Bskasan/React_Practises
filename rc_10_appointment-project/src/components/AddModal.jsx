ada8bf6420ebbb40513f4bcd9c6f6d1423c675bb
Bskasan <b.kasan@yandex.com>
2023-03-16 15:04:10 +0200

    setDate and patient name

--- rc_10_appointment-project/src/components/AddModal.jsx
+++ rc_10_appointment-project/src/components/AddModal.jsx
@@ -4,6 +4,16 @@ import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 
 export function AddModal({ show, handleClose, drName }) {
+  const [patientName, setPatientName] = useState("");
+  const [date, setDate] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //----
+    handleClose();
+  };
+
+  console.log(patientName, date);
   return (
     <>
       <Modal show={show} onHide={handleClose}>
@@ -14,12 +24,19 @@ export function AddModal({ show, handleClose, drName }) {
           <Form>
             <Form.Group className="mb-3" controlId="name">
               <Form.Label>Patient Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter your name" />
+              <Form.Control
+                type="text"
+                placeholder="Enter your name"
+                onChange={(e) => setPatientName(e.target.value)}
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="datetime">
               <Form.Label>Day & Time</Form.Label>
-              <Form.Control type="datetime-local" />
+              <Form.Control
+                type="datetime-local"
+                onChange={(e) => setDate(e.target.value)}
+              />
             </Form.Group>
 
             <div className="text-center">
@@ -27,12 +44,12 @@ export function AddModal({ show, handleClose, drName }) {
                 variant="success"
                 type="submit"
                 className="me-2"
-                onClick={handleClose}
+                onClick={handleSubmit}
               >
-                Close
+                Save
               </Button>
               <Button variant="danger" onClick={handleClose}>
-                Save Changes
+                Close
               </Button>
             </div>
           </Form>
