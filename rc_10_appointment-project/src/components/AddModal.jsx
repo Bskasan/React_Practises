cf485fc2a1716f754cf247b18a3e0f610ce60b7d
Bskasan <b.kasan@yandex.com>
2023-03-16 14:29:21 +0200

    model setting 1

--- rc_10_appointment-project/src/components/AddModal.jsx
+++ rc_10_appointment-project/src/components/AddModal.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
-const AddModal = () => {
+export function AddModal({ show, handleClose }) {
   return (
-    <div>AddModal</div>
-  )
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
-
-export default AddModal
--- rc_10_appointment-project/src/components/Doctors.jsx
+++ rc_10_appointment-project/src/components/Doctors.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 //! Better for Performance to add bootstrap components.
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { AddModal } from "./AddModal";
 
 const Doctors = ({ doctors }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => {
+    setShow(true);
+  };
+
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
@@ -13,12 +20,19 @@ const Doctors = ({ doctors }) => {
       <Row className="justify-content-center">
         {doctors.map((dr) => (
           <Col key={dr.id} xs={6} sm={4} md={3}>
-            <img src={dr.img} alt={dr.name} className="img-thumbnail doctor-img" />
-            <h4>{dr.name}</h4>
-            <h5>{dr.dep}</h5>
+            <img
+              src={dr.img}
+              alt={dr.name}
+              className="img-thumbnail doctor-img"
+              onClick={handleClick}
+            />
+            <h5>{dr.name}</h5>
+            <h6>{dr.dep}</h6>
           </Col>
         ))}
       </Row>
+
+      <AddModal show={show} handleClose={() => setShow(false)} />
     </Container>
   );
 };
