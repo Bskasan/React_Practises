2e70042080314de4664447012fb97aef21af6ae6
Bskasan <b.kasan@yandex.com>
2023-03-16 14:38:23 +0200

    modal setting buttons added

--- rc_10_appointment-project/src/components/AddModal.jsx
+++ rc_10_appointment-project/src/components/AddModal.jsx
@@ -12,34 +12,31 @@ export function AddModal({ show, handleClose }) {
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Patient Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter your name" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+            <Form.Group className="mb-3" controlId="datetime">
+              <Form.Label>Day & Time</Form.Label>
+              <Form.Control type="datetime-local" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
+
+            <div className="text-center">
+              <Button
+                variant="success"
+                type="submit"
+                className="me-2"
+                onClick={handleClose}
+              >
+                Close
+              </Button>
+              <Button variant="danger" onClick={handleClose}>
+                Save Changes
+              </Button>
+            </div>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
       </Modal>
     </>
   );
