a8e3f5e3cae40afd1b6cf4a7f529216de6345801
Bskasan <b.kasan@yandex.com>
2023-03-17 19:47:34 +0200

    react-router-dom added

--- rc_11_router-intro/router-intro-inclass/src/App.js
+++ rc_11_router-intro/router-intro-inclass/src/App.js
@@ -4,17 +4,20 @@ import Home from "./pages/Home";
 import Instructors from "./pages/Instructors";
 import NotFound from "./pages/NotFound";
 import Nav from "./components/Nav";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Nav />
-      <Home />
-      <Instructors />
-      <Contact />
-      <NotFound />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/instructors" element={<Instructors />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
