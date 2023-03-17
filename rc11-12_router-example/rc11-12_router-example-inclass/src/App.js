1e35644e423c237817221bae988d7fccb07ca6a8
Bskasan <b.kasan@yandex.com>
2023-03-17 20:26:05 +0200

    react-router added

--- rc11-12_router-example/rc11-12_router-example-inclass/src/App.js
+++ rc11-12_router-example/rc11-12_router-example-inclass/src/App.js
@@ -1,10 +1,23 @@
-
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import People from "./pages/People";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
