7afafb8e01e8b1328b19a38fa3889c173df55d65
Bskasan <b.kasan@yandex.com>
2023-03-17 20:02:01 +0200

    link navlink

--- rc_11_router-intro/router-intro-inclass/src/components/Nav.jsx
+++ rc_11_router-intro/router-intro-inclass/src/components/Nav.jsx
@@ -1,14 +1,21 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Nav = () => {
   return (
     <ul>
       <li>
-        <a href="/">Home</a>
+        <Link to="/">Home</Link>
       </li>
       <li>
-        <a href="/instructors">Instructors</a>
+        <Link to="/instructors">Instructors</Link>
       </li>
       <li>
-        <a href="/contact">Contact</a>
+        <NavLink
+          to="/contact"
+          style={({ isActive }) => ({ color: isActive && "red" })}
+        >
+          Contact
+        </NavLink>
       </li>
     </ul>
   );
