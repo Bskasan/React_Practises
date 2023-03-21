595697db3a0b83a65f9b5730ce7c97034577d44d
Bskasan <b.kasan@yandex.com>
2023-03-22 00:28:42 +0200

    flex

--- rc_13_styled-comp-example/src/components/Header.jsx
+++ rc_13_styled-comp-example/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./styled/Button";
+import Flex from "./styled/Flex";
 import Nav from "./styled/Nav";
 
 const Header = () => {
@@ -16,7 +17,17 @@ const Header = () => {
         </div>
       </Nav>
 
-      
+      <Flex>
+        <div>
+          <h1>The IT Career of Your Dreams Starts Here!</h1>
+          <p>
+            Clarusway is a leading international software Bootcamp. Join a micro
+            class online with other trainees and learn coding skills with a
+            highly-skilled instructor.
+          </p>
+          <Button bg="#AF3A53">Start Your New Career</Button>
+        </div>
+      </Flex>
     </div>
   );
 };
---
+++ rc_13_styled-comp-example/src/components/styled/Flex.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+`;
+
+export default Flex;
