2586ff526a1d848aaaea49c85ebe0c4d10ffe9f9
Bskasan <b.kasan@yandex.com>
2023-03-21 23:31:16 +0200

    links styled comp added

--- rc_13_styled-comp-intro/styled-intro_inclass/src/App.js
+++ rc_13_styled-comp-intro/styled-intro_inclass/src/App.js
@@ -1,19 +1,28 @@
 import { Button, ExtButton } from "./components/Button.styled";
 import HeaderText from "./components/HeaderText";
+import Links from "./components/Links";
 import Sarmal from "./components/Sarmal";
 
 function App() {
   return (
-    <Sarmal bg="#dee">
-      <HeaderText color="hotpink">Styled-Components</HeaderText>
-      <HeaderText color="#734586" bgColor="#eee">
-        Styled-Components
-      </HeaderText>
-      <Button>Send</Button>
-      <Button primary>Send</Button>
-      <ExtButton react>Study</ExtButton>
-      <ExtButton>Study More!</ExtButton>
-    </Sarmal>
+    <>
+      <Sarmal bg="#dee">
+        <HeaderText color="hotpink">Styled-Components</HeaderText>
+        <HeaderText color="#734586" bgColor="#eee">
+          Styled-Components
+        </HeaderText>
+        <Button>Send</Button>
+        <Button primary>Send</Button>
+        <ExtButton react>Study</ExtButton>
+        <ExtButton>Study More!</ExtButton>
+      </Sarmal>
+
+      <Sarmal>
+        <Links href="https://github.com/Bskasan" target="_blank">
+          --- Bekir's Personal Website ---
+        </Links>
+      </Sarmal>
+    </>
   );
 }
 
---
+++ rc_13_styled-comp-intro/styled-intro_inclass/src/components/Links.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Links = styled.a`
+  text-decoration: none;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1.1rem;
+  margin-top: 1rem;
+  &:hover {
+    font-weight: 700;
+  }
+`;
+
+export default Links;
