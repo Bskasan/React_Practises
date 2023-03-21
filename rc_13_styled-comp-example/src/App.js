0d799cb56de03c1f44b3d01e7e15603c949a5c09
Bskasan <b.kasan@yandex.com>
2023-03-22 00:54:39 +0200

    used themeprovider

--- rc_13_styled-comp-example/src/App.js
+++ rc_13_styled-comp-example/src/App.js
@@ -1,15 +1,27 @@
 import Header from "./components/Header";
 import Container from "./components/styled/Container";
 import { GlobalStyles } from "./components/styled/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+
+const styles = {
+  colors: {
+    primary: "#444",
+    secondary: "#666",
+  },
+  margins: {},
+
+  paddings: {},
+  breakpoints: { xs: "300px", sm: "500px", md: "700px" },
+};
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={styles}>
       <GlobalStyles />
       <Container>
         <Header />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
--- rc_13_styled-comp-example/src/components/styled/Nav.jsx
+++ rc_13_styled-comp-example/src/components/styled/Nav.jsx
@@ -5,8 +5,9 @@ const Nav = styled.nav`
   justify-content: space-between;
   align-items: center;
   padding: 1.5rem;
+  background-color: ${({ theme }) => theme.colors.primary};
 
-  @media (max-width: 700px) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.md}) {
     flex-direction: column;
   }
 
