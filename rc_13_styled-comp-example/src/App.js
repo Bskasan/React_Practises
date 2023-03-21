4ac80dca5165b45763b19fe4b73c42f4ba1b235d
Bskasan <b.kasan@yandex.com>
2023-03-22 00:17:53 +0200

    nav styling

--- rc_13_styled-comp-example/src/App.js
+++ rc_13_styled-comp-example/src/App.js
@@ -1,10 +1,10 @@
+import Header from "./components/Header";
 import Button from "./components/styled/Button";
 
 function App() {
   return (
     <>
-      <Button color="#AF3A53"> Apply Courses</Button>
-      <Button bg="#AF3A53"> Talk to Advisor</Button>
+      <Header />
     </>
   );
 }
--- rc_13_styled-comp-example/src/components/Header.jsx
+++ rc_13_styled-comp-example/src/components/Header.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import Button from "./styled/Button";
+import Nav from "./styled/Nav";
 
 const Header = () => {
   return (
     <div>
-        
-    </div>
-  )
-}
+      <Nav>
+        <div>
+          <img src="./images/logo.png" width={"300px"} alt="logo" />
+        </div>
 
-export default Header
+        <div>
+          <Button color="#AF3A53"> Apply Courses</Button>
+          <Button bg="#AF3A53"> Talk to Advisor</Button>
+        </div>
+      </Nav>
+    </div>
+  );
+};
+
+export default Header;
