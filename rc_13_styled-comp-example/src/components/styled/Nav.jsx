f5892016bde34d5bb5af0703604d751dbd2145c1
Bskasan <b.kasan@yandex.com>
2023-03-22 00:20:23 +0200

    max width added

--- rc_13_styled-comp-example/src/components/styled/Nav.jsx
+++ rc_13_styled-comp-example/src/components/styled/Nav.jsx
@@ -2,9 +2,12 @@ import styled from "styled-components";
 
 const Nav = styled.nav`
   display: flex;
-  flex-direction: row;
   justify-content: space-between;
   align-items: center;
+
+  @media (max-width: 700px) {
+    flex-direction: column;
+  }
 `;
 
 export default Nav;
