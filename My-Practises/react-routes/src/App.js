8b8504773d5a592b361d27d0fd67b54a719077b6
Bskasan <b.kasan@yandex.com>
2023-03-23 01:20:34 +0200

    links added to nav

--- My-Practises/react-routes/src/App.js
+++ My-Practises/react-routes/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
@@ -8,11 +8,19 @@ import ErrorPage from "./pages/ErrorPage";
 function App() {
   return (
     <BrowserRouter>
+      <nav>
+        <h2>Bekir's Web Page</h2>
+        <div>
+          <Link to="/"> Home </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/profile"> Profile </Link>
+        </div>
+      </nav>
       <h1>Hello World</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />     
+        <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
