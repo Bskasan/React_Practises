884a8c8bba5e7dac8bcf318d819a9ca80bb39a38
Bskasan <b.kasan@yandex.com>
2023-03-23 01:21:49 +0200

    about me added

--- My-Practises/react-routes/src/pages/About.jsx
+++ My-Practises/react-routes/src/pages/About.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>THIS IS THE ABOUT PAGE</div>
-  )
-}
+    <div>
+      <h1>THIS IS ABOUT PAGE</h1>
+      <p>
+        Passionate Software developer (Front-end Developer)with over 3 years in
+        the industry, including 1+ years of professional experience. Skilled in
+        game development using Unity and C#, with a strong understanding of
+        object-oriented programming, data structures, and the software
+        development lifecycle. Recently pivoted to front-end web development,
+        mastering HTML, CSS, JavaScript, and React. Ready to bring my passion
+        and skills to a new challenge as a front-end web developer.
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
