184cec1e08ba07de37bbc77bd37e1c64cabb4649
Bskasan <b.kasan@yandex.com>
2023-03-24 23:32:18 +0200

    setErrorState and setLoading

--- rc14-15_clarus-library/checkout-page/src/pages/ProductList.jsx
+++ rc14-15_clarus-library/checkout-page/src/pages/ProductList.jsx
@@ -5,19 +5,25 @@ import axios from "axios";
 
 const ProductList = () => {
   const url = process.env.REACT_APP_API_URL;
-  console.log("url", url);
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [errorState, setErrorState] = useState(false);
 
   const getProducts = async () => {
     console.log("merhaba");
     try {
       const { data } = await axios(url);
       setProducts(data);
+      setLoading(false); //? try a girdiğinde loadingi false a çek diyorum ki sonucu bilebileyim
+      setErrorState(false); //? işlem başarılı olursa erroru falsea çekiyorum
+      console.log(data);
     } catch (error) {
       console.log(error);
+      setErrorState(true); //? catche düştüğünde errorstate ini true değerine çeviriyorum
     }
   };
 
+  //? componentDidMount
   useEffect(() => {
     getProducts();
   }, []);
@@ -25,16 +31,23 @@ const ProductList = () => {
   return (
     <div className="container mt-3">
       <div className="bg-light d-sm-block d-md-flex">
-        <p className="text-center text-danger w-100">Loading...</p>
-        <>
-          <article id="product-panel" className="col-md-5">
-            <ProductCard />
-          </article>
-          <article className="col-md-5 m-3">
-            <CardTotal />
-          </article>
-        </>
-        <p className="text-center text-danger w-100">No products data...</p>
+        {loading ? (
+          <p className="text-center text-danger w-100">Loading...</p>
+        ) : products.length > 0 ? (
+          <>
+            <article id="product-panel" className="col-md-5">
+              <ProductCard />
+            </article>
+            <article className="col-md-5 m-3">
+              <CardTotal />
+            </article>
+          </>
+        ) : (
+          <p className="text-center text-danger w-100">No products data...</p>
+        )}
+        {errorState && (
+          <p className="text-center text-danger w-100">Error...</p>
+        )}
       </div>
     </div>
   );
