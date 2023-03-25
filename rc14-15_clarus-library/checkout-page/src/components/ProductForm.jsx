6385adbd3a6903aa46cfe6a0c91a3f7727371dcc
Bskasan <b.kasan@yandex.com>
2023-03-26 00:03:37 +0200

    new product 1

--- rc14-15_clarus-library/checkout-page/src/components/ProductForm.jsx
+++ rc14-15_clarus-library/checkout-page/src/components/ProductForm.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
-
-const ProductForm = () => {
+const ProductForm = ({ formData, handleChange, handleSubmit, text }) => {
   return (
-    <div>ProductForm</div>
-  )
-}
+    <article id="add-product" className="mb-4 mt-4">
+      <h1 className="text-center">Product</h1>
+      <form class="p-2">
+        <div class="mb-3">
+          <label for="add-name" class="form-label">
+            Product Name
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div class="mb-3">
+          <label for="add-price" class="form-label">
+            Product Price
+          </label>
+          <input type="number" class="form-control" id="price" required />
+        </div>
+        <div class="mb-3">
+          <label for="add-quantity" class="form-label">
+            Product Quantity
+          </label>
+          <input type="number" class="form-control" id="amount" required />
+        </div>
+        <label for="add-image" class="form-label">
+          Product Image
+        </label>
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="basic-addon3">
+            https://example.com/
+          </span>
+          <input
+            type="url"
+            class="form-control"
+            id="image"
+            aria-describedby="basic-addon3"
+            required
+          />
+        </div>
+        <div class="text-center">
+          <button type="submit" class="add-to-cart btn btn-success btn-sm">
+            <i class="fa-solid fa-cart-plus me-2"></i>Save To Product
+          </button>
+        </div>
+      </form>
+    </article>
+  );
+};
 
-export default ProductForm
+export default ProductForm;
--- rc14-15_clarus-library/checkout-page/src/pages/NewProduct.jsx
+++ rc14-15_clarus-library/checkout-page/src/pages/NewProduct.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import ProductForm from "../components/ProductForm";
+
+//? Initial State Object
+const initialState = {
+  name: "",
+  image: "",
+  price: "",
+  dampingRate: 0.8,
+  amount: 1,
+};
 
 const NewProduct = () => {
-  return (
-    <div>NewProduct</div>
-  )
-}
+  const url = process.env.REACT_APP_API_URL;
+  const [formData, setFormData] = useState(initialState);
 
-export default NewProduct
+  handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+    //! change eventının gerçekleştiği inputtaki id attribute u ile formDatamdaki key değerlerim aynı olduğu için dinamik bir şekilde formData mı güncelleybiliyorum
+    console.log({ [e.target.id]: e.target.value });
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(url, formData);
+      setFormData(initialState);
+    } catch (error) {}
+  };
+
+  return (
+    <div className="container">
+      <ProductForm
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        formData={formData}
+      />
+    </div>
+  );
+};
+
+export default NewProduct;
