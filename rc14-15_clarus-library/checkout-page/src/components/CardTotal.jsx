5228d659846e21f1cfb53beb60dee0588d8adbc7
Bskasan <b.kasan@yandex.com>
2023-03-25 23:05:07 +0200

    api bug fixed

--- rc14-15_clarus-library/checkout-page/src/components/CardTotal.jsx
+++ rc14-15_clarus-library/checkout-page/src/components/CardTotal.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
 
-const CardTotal = () => {
+const taxRate = 0.18;
+const shipping = 25;
+
+const CardTotal = ({ products }) => {
+  const subTotal = products.reduce(
+    (acc, product) =>
+      product.price * product.amount * product.dampingRate + acc,
+    0
+  );
+
+  console.log("SubTotal: ", subTotal);
   return (
-    <div>CardTotal</div>
-  )
-}
+    <table className="table w-100">
+      <tbody>
+        <tr className="text-end">
+          <th className="text-start">Subtotal</th>
+          <td>
+            $<span className="subtotal">{subTotal.toFixed(2)}</span>
+          </td>
+        </tr>
+        <tr className="text-end">
+          <th className="text-start">Tax(18%)</th>
+          <td>
+            $<span className="tax">{(subTotal * taxRate).toFixed(2)}</span>
+          </td>
+        </tr>
+        <tr className="text-end">
+          <th className="text-start">Shipping</th>
+          <td>
+            $<span className="shipping">{shipping}</span>
+          </td>
+        </tr>
+        <tr className="text-end">
+          <th className="text-start">Total</th>
+          <td>
+            $
+            <span className="total">
+              {(subTotal + shipping + taxRate * subTotal).toFixed(2)}
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 
-export default CardTotal
+export default CardTotal;
