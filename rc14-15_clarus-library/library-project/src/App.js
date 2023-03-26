fa85e282eebbe49ee9d8c99db1ca0eb57d93e4d3
Bskasan <b.kasan@yandex.com>
2023-03-27 01:02:14 +0300

    app router done

--- rc14-15_clarus-library/library-project/src/App.js
+++ rc14-15_clarus-library/library-project/src/App.js
@@ -1,12 +1,11 @@
 import AppRouter from "./router/AppRouter";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
-import { GlobalStyles } from "./styles/Global.styles";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
+      {/* <GlobalStyles /> */}
       <AppRouter />
     </ThemeProvider>
   );
--- rc14-15_clarus-library/library-project/src/router/AppRouter.jsx
+++ rc14-15_clarus-library/library-project/src/router/AppRouter.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GlobalStyles } from "../styles/Global.styles";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
+import PrivateRouter from "./PrivateRouter";
+import Detail from "../pages/detail/Detail";
+import About from "../pages/about/About";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <GlobalStyles />
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/about" element={<PrivateRouter />}>
+          <Route path="" element={<About />} />
+        </Route>
+        <Route path="/detail" element={<PrivateRouter />}>
+          <Route path="" element={<Detail />} />
+        </Route>
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
