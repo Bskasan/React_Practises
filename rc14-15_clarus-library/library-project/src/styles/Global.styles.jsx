af7d85897aaab067fdcb2c85ad8626339cbb54a6
Bskasan <b.kasan@yandex.com>
2023-03-27 00:48:58 +0300

    theme and glabal styles

--- rc14-15_clarus-library/library-project/src/styles/Global.styles.jsx
+++ rc14-15_clarus-library/library-project/src/styles/Global.styles.jsx
@@ -1,1 +1,16 @@
-//create global styles
+//create global style
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        text-transform: uppercase;
+        font-family: 'Shantell Sans', cursive;
+    }
+    body{
+        font-size: 1.3rem;
+        background-color: ${({ theme }) => theme.colors.mainColor};
+    }
+`;
--- rc14-15_clarus-library/library-project/src/styles/theme.js
+++ rc14-15_clarus-library/library-project/src/styles/theme.js
@@ -1,1 +1,14 @@
-//create theme
+//? CREATE THEME
+export const theme = {
+  colors: {
+    navbarBgColor: "white",
+    mainColor: "#86C8BC",
+    logoColor: "darkslategray",
+    linkColor: "#8B7E74",
+    linkHoverColor: "rgb(5,5,5)",
+    headerFormColor: "rgba(15, 63, 42, 0.36)",
+  },
+  screens: {
+    lg: "992px",
+  },
+};
