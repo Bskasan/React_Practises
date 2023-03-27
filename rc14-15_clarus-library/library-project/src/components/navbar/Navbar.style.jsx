ee9e61a970897f6bb45ba3adbe9de4d3fe2f4feb
Bskasan <b.kasan@yandex.com>
2023-03-27 18:53:12 +0300

    menu and menu link styled comp added

--- rc14-15_clarus-library/library-project/src/components/navbar/Navbar.style.jsx
+++ rc14-15_clarus-library/library-project/src/components/navbar/Navbar.style.jsx
@@ -14,3 +14,19 @@ export const Logo = styled(NavLink)`
   font-weight: 800;
   font-size: 2rem;
 `;
+
+export const Menu = styled(Flex)`
+  font-weight: bold;
+`;
+
+export const MenuLink = styled(NavLink)`
+  text-align: center;
+  padding: 1rem 2rem;
+  cursor: pointer;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.linkColor};
+  transition: all 0.3s ease-in;
+  :hover {
+    color: ${({ theme }) => theme.colors.linkHoverColor};
+  }
+`;
