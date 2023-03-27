386732069add992537319089ca882737dd1732f3
Bskasan <b.kasan@yandex.com>
2023-03-27 22:58:52 +0300

    styled input and button

--- rc14-15_clarus-library/library-project/src/pages/login/Login.style.jsx
+++ rc14-15_clarus-library/library-project/src/pages/login/Login.style.jsx
@@ -16,3 +16,40 @@ export const FormContainer = styled(Flex)`
   padding: 0.5rem;
   flex-direction: column;
 `;
+
+export const StyledInput = styled.input`
+  display: block;
+  height: 50px;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 15px;
+  padding: 0 10px;
+  margin-top: 8px;
+  font-size: 14px;
+  font-weight: 300;
+  outline: none;
+  border: none;
+  ::placeholder {
+    color: black;
+  }
+  :focus {
+    background-color: white;
+  }
+`;
+
+export const StyledButton = styled.button`
+  margin-top: 50px;
+  background-color: ${({ theme }) => theme.colors.navbarBgColor};
+  color: ${({ theme }) => theme.colors.logoColor};
+  padding: 15px 0;
+  opacity: 0.5;
+  font-size: 18px;
+  font-weight: 600;
+  border-radius: 15px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  &:hover {
+    opacity: 1;
+  }
+`;
