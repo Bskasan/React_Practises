a849e7a88d6d945f3609d9d74d5cb63ac0dc9bd5
Bskasan <b.kasan@yandex.com>
2023-03-28 23:59:56 +0300

    created login context basic structure

--- rv16_context-api/context-example-1/src/App.js
+++ rv16_context-api/context-example-1/src/App.js
@@ -6,23 +6,31 @@ import People from "./pages/People";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import PersonDetail from "./pages/PersonDetail";
 import Login from "./pages/Login";
+import { LoginContext } from "./context/LoginContext";
+
+//? Global State Management
+//? - Context API (Small project, data flow slow, if there aren't lot of global state)
+//? - Redux
+//? - Mobx
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navs />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="login" element={<Login />} />
+    <LoginContext.Provider value={{}}>
+      <BrowserRouter>
+        <Navs />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="login" element={<Login />} />
 
-        <Route path="people" element={<People />} />
-        <Route path="people/:id" element={<PersonDetail />} />
+          <Route path="people" element={<People />} />
+          <Route path="people/:id" element={<PersonDetail />} />
 
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </LoginContext.Provider>
   );
 }
 
