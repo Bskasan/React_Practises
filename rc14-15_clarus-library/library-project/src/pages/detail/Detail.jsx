a46b4ba0b445517f4541d8b4bf4d8b48dd9c9abc
Bskasan <b.kasan@yandex.com>
2023-03-28 18:40:05 +0300

    detail page done

--- rc14-15_clarus-library/library-project/src/pages/detail/Detail.jsx
+++ rc14-15_clarus-library/library-project/src/pages/detail/Detail.jsx
@@ -1,14 +1,41 @@
 import React from "react";
 import { useLocation, useParams } from "react-router-dom";
+import {
+  Description,
+  DetailContainer,
+  DetailImg,
+  DetailTitle,
+  InfoPart,
+} from "./Detail.style";
+import defaultImage from "../../assets/book.jpg";
 
 const Detail = () => {
   const { state } = useLocation();
   const { id } = useParams();
 
-  // console.log(id);
-  // console.log(state);
+  console.log(id);
+  console.log(state);
 
-  return <div>Detail</div>;
+  return (
+    <DetailContainer>
+      <DetailTitle>{state.volumeInfo.title}</DetailTitle>
+      <DetailImg
+        src={state.volumeInfo.imageLinks?.smallThumbnail || defaultImage}
+        alt={state.volumeInfo.title}
+      />
+      <Description>{state.volumeInfo.description}</Description>
+      <InfoPart>
+        <p>
+          {state.volumeInfo.authors?.map((item) => (
+            <span key={item}>{item}</span>
+          ))}
+        </p>
+        <p>
+          {state.volumeInfo.publishedDate} / {state.volumeInfo.publisher}
+        </p>
+      </InfoPart>
+    </DetailContainer>
+  );
 };
 
 export default Detail;
