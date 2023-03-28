165dc0852766b8ee7cf14631f32723bd0a90f05d
Bskasan <b.kasan@yandex.com>
2023-03-28 19:19:13 +0300

    footer and its styled comp added

--- rc14-15_clarus-library/library-project/src/components/footer/Footer.jsx
+++ rc14-15_clarus-library/library-project/src/components/footer/Footer.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { FooterBtn, FooterContainer } from "./Footer.style";
+import { lightIcon, darkIcon } from "../../helper/iconData";
 
-const Footer = () => {
+const Footer = ({ myTheme, setMyTheme }) => {
+  const handleClick = () => {
+    if (myTheme === "light") {
+      setMyTheme("dark");
+    } else {
+      setMyTheme("light");
+    }
+  }; //!myTheme ya göre güncelleme yapacak kullanıcı tıkladıkça toggle mantığı kurmuş olduk
+
   return (
-    <div>Footer</div>
-  )
-}
+    <FooterContainer>
+      <FooterBtn onClick={handleClick} title="Switch theme">
+        {myTheme === "light" ? <>{lightIcon}</> : <>{darkIcon}</>}
+      </FooterBtn>
+    </FooterContainer>
+  );
+};
 
-export default Footer
+export default Footer;
---
+++ rc14-15_clarus-library/library-project/src/components/footer/Footer.style.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const FooterBtn = styled.button`
+  border: none;
+  outline: none;
+  background: none;
+  cursor: pointer;
+  position: fixed;
+  bottom: 0px;
+`;
+export const FooterContainer = styled.div`
+  position: relative;
+  margin: 1rem;
+`;
