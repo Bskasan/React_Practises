839801915a572b3d277a2c2a0ea638feea05f53d
Bskasan <b.kasan@yandex.com>
2023-03-30 22:38:27 +0300

    set up done tailwind

--- rc17_movie-app/src/pages/Register.jsx
+++ rc17_movie-app/src/pages/Register.jsx
@@ -1,34 +1,61 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import GoogleIcon from "../assets/icons/GoogleIcon";
 
 const Register = () => {
   return (
     <div className="overflow-hidden flex-1 h-screen justify-center items-center bg-[#23242a]">
-      <div
-        className={`mt-[3vh] mx-auto overflow-hidden relative w-[380px] h-[620px] rounded-[8px] bg-[#1c1c1c] before:content-[""] before:absolute before:w-[380px] before:h-[420px] before:top-[-50%] before:left-[-50%] after:content-[""] after:absolute after:w-[380px] after:h-[420px] after:top-[-50%] after:left-[-50%] custom-linear-gradient`}
-      >
-        <form className="absolute inset-[2px] rounded-[8px] bg-gray-100 dark:bg-[#28292d] z-[10] flex flex-col py-[50px] px-[40px]">
+      <div className={`form-container mt-[5vh] w-[380px] h-[580px]`}>
+        <form>
           <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3">
             Sign Up
           </h2>
-          <div>
-            <input name="floating_text" type="text" required />
-            <label htmlFor="floating_text">First Name</label>
+          <div className="relative z-0 w-full mb-6 group">
+            <input
+              type="text"
+              name="floating_text"
+              className="peer"
+              placeholder=" "
+              required
+            />
+            <label htmlFor="floating_email">First Name</label>
           </div>
-          <div>
-            <input name="floating_text" type="text" required />
+          <div className="relative z-0 w-full mb-6 group">
+            <input
+              name="floating_text"
+              type="text"
+              required
+              className="peer"
+              placeholder=" "
+            />
             <label htmlFor="floating_text">Last Name</label>
           </div>
-          <div>
-            <input name="floating_email" type="email" />
+          <div className="relative z-0 w-full mb-6 group">
+            <input
+              name="floating_email"
+              type="email"
+              className="peer"
+              placeholder=" "
+              required
+            />
             <label htmlFor="floating_email">Email</label>
           </div>
-          <div>
-            <input name="floating_password" type="password" />
+          <div className="relative z-0 w-full mb-6 group">
+            <input
+              name="floating_password"
+              type="password"
+              className="peer"
+              placeholder=" "
+              required
+            />
             <label htmlFor="floating_password">Password</label>
           </div>
-          <button type="submit">Register</button>
-          <button type="button">
+          <button className="btn-danger" type="submit">
+            Register
+          </button>
+          <button
+            className="flex justify-between text-center btn-danger"
+            type="button"
+          >
             Continue with Google
             <GoogleIcon color="currentColor" />
           </button>
