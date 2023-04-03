cdebc6ec89d0b46a157c4ad1f3b68db61993657e
Bskasan <b.kasan@yandex.com>
2023-04-04 00:41:44 +0300

    getMovies and loading done

--- rc17_movie-app/src/pages/Main.jsx
+++ rc17_movie-app/src/pages/Main.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MovieContext } from "../context/MovieContext";
 
 const Main = () => {
-  return <div>Main</div>;
+  const { movies } = useContext(MovieContext);
+  return (
+    <div>
+      <div className="flex justify-center flex-wrap">
+        {movies.map((movie) => console.log(movie))}
+      </div>
+    </div>
+  );
 };
 
 export default Main;
