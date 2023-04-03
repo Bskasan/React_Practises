2c72723a2572b65256a75eee2361f15ff0e397fe
Bskasan <b.kasan@yandex.com>
2023-04-04 01:10:30 +0300

    setSearchTerm onChange

--- rc17_movie-app/src/pages/Main.jsx
+++ rc17_movie-app/src/pages/Main.jsx
@@ -4,8 +4,20 @@ import MovieCard from "../components/MovieCard";
 
 const Main = () => {
   const { movies, loading } = useContext(MovieContext);
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div>
+      <form className="flex justify-center p-2" >
+        <input
+          type="search"
+          className="w-80 h-8 rounded-md p-1 m-2"
+          placeholder="Search a movie..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button className="btn-danger-bordered" type="submit">
+          Search
+        </button>
+      </form>
       <div className="flex justify-center flex-wrap">
         {loading ? (
           <div
