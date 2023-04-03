3d2aa43773e1d9a12a05b951f75206138d8113f8
Bskasan <b.kasan@yandex.com>
2023-04-03 17:57:14 +0300

    react toast

--- rc17_movie-app/src/App.js
+++ rc17_movie-app/src/App.js
@@ -4,12 +4,14 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
 import AuthContextProvider from "./context/AuthContext";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   return (
     <div>
       <AuthContextProvider>
         <AppRouter />
+        <ToastContainer />
       </AuthContextProvider>
     </div>
   );
