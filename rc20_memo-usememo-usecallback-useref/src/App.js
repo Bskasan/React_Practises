70575f5e28611ac99d765eab696c343f6db76039
Bskasan <b.kasan@yandex.com>
2023-04-08 23:06:48 +0300

    memo second method

--- rc20_memo-usememo-usecallback-useref/src/App.js
+++ rc20_memo-usememo-usecallback-useref/src/App.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import HeaderMemo from "./components/HeaderMemo";
+import TaxComp from "./components/TaxComp";
 
 function App() {
   const [count, setCount] = useState(0);
   const [user, setUser] = useState("Bekir");
+
+  const taxData = { taxRate: 0.18, ship: 25 };
 
   return (
     <div className="container mt-2">
@@ -28,7 +31,10 @@ function App() {
         </button>
       </div>
       <hr />
-      <HeaderMemo user={user} />
+      {/* <HeaderMemo user={user} /> */}
+      <HeaderMemo count={count} />
+      <hr />
+      <TaxComp />
     </div>
   );
 }
--- rc20_memo-usememo-usecallback-useref/src/components/HeaderMemo.jsx
+++ rc20_memo-usememo-usecallback-useref/src/components/HeaderMemo.jsx
@@ -1,12 +1,24 @@
 import React, { memo } from "react";
 
-const HeaderMemo = memo(({ user }) => {
+// const HeaderMemo = memo(({ user }) => {
+//   console.log("Render => HeaderMemo Component");
+//   return (
+//     <div className="text-center text-success">
+//       HeaderMemo Componenti: {user}
+//     </div>
+//   );
+// });
+
+// export default HeaderMemo;
+
+//memo second method
+const HeaderMemo = ({ count }) => {
   console.log("Render => HeaderMemo Component");
   return (
     <div className="text-center text-success">
-      HeaderMemo Componenti: {user}
+      HeaderMemo Componenti: {count}
     </div>
   );
-});
+};
 
-export default HeaderMemo;
+export default memo(HeaderMemo);
