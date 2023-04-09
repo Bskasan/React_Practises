32f668fb95d9245204fd5d1f1c53a6ef7ea1b039
Bskasan <b.kasan@yandex.com>
2023-04-10 00:52:50 +0300

    decrease and reset cases

--- rc21_redux-intro/src/redux/index.jsx
+++ rc21_redux-intro/src/redux/index.jsx
@@ -2,12 +2,17 @@ const initialState = {
   counter: 0, //? Global state...
 };
 
+//? Pure js function
+//? Bir dis bagimlilik olmamali
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "INCREASE":
       return { counter: state.counter + 1 };
-
+    case "DECREASE":
+      return { counter: state.counter - 1 };
+    case "RESET":
+      return { counter: 0 };
     default:
-      break;
+      return state;
   }
 };
