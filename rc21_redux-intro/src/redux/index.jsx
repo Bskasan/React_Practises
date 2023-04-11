6e67e3685299ebb0288dd1b2af6238124fb3265e
Bskasan <b.kasan@yandex.com>
2023-04-11 21:32:42 +0300

    continue the class

--- rc21_redux-intro/src/redux/index.jsx
+++ rc21_redux-intro/src/redux/index.jsx
@@ -18,3 +18,15 @@
 // };
 
 // export default reducer;
+
+import { legacy_createStore as createStore, combineReducers } from "redux";
+import counterReducer from "./reducers/counterReducer";
+import todoReducer from "./reducers/todoReducer";
+
+//? Packaging Reducers
+const rootReducer = combineReducers({
+  count: counterReducer,
+  todo: todoReducer,
+});
+
+export const store = createStore(rootReducer);
