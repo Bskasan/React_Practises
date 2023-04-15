f2d359812265b2ae0655ef8ae6bb01925fd61d9c
Bskasan <b.kasan@yandex.com>
2023-04-16 00:33:13 +0300

    stock app v2 started

---
+++ rc23-28_stock-app-v-2/src/hooks/useAuthCall.jsx
@@ -0,0 +1,66 @@
+import axios from "axios";
+import {
+  fetchFail,
+  fetchStart,
+  loginSuccess,
+  logoutSuccess,
+  registerSuccess,
+} from "../features/authSlice";
+
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
+
+const useAuthCall = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const BASE_URL = "https://12177.fullstack.clarusway.com/redoc/";
+
+  const login = async (userInfo) => {
+    dispatch(fetchStart());
+    try {
+      const { data } = await axios.post(
+        `${BASE_URL}account/auth/login/`,
+        userInfo
+      );
+      dispatch(loginSuccess(data));
+      toastSuccessNotify("Login performed");
+      navigate("/stock");
+      console.log(data);
+    } catch (error) {
+      dispatch(fetchFail());
+      console.log(error);
+    }
+  };
+
+  const logout = async () => {
+    dispatch(fetchStart());
+    try {
+      await axios.post(`${BASE_URL}account/auth/logout/`);
+      dispatch(logoutSuccess());
+      toastSuccessNotify("Logout performed");
+      navigate("/");
+    } catch (err) {
+      dispatch(fetchFail());
+      toastErrorNotify("Logout can not be performed");
+    }
+  };
+
+  const register = async (userInfo) => {
+    dispatch(fetchStart());
+    try {
+      const { data } = await axios.post(
+        `${BASE_URL}account/register/`,
+        userInfo
+      );
+      dispatch(registerSuccess(data));
+      toastSuccessNotify("Register performed");
+      navigate("/stock");
+    } catch (err) {
+      dispatch(fetchFail());
+      toastErrorNotify("Register can not be performed");
+    }
+  };
+
+  return { login, register, logo
